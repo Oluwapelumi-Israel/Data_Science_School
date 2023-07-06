f1f8a2f93095f5a828b6133d9902396220782a1c
Oluwapelumi-Israel <israeloluwapelumi@gmail.com>
2023-07-06 08:53:38 +0100

    Started_The_Project_And_Added_New_Files

--- index.php
+++ index.php
@@ -2,10 +2,71 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATA SCIENCE SCHOOL</title>
+    <link rel="stylesheet" href="Assets/font.css">
+    <link rel="stylesheet" href="Assets/data_science_school.css">
+    <script src="https://kit.fontawesome.com/e0930b8f2a.js"></script>
 </head>
 <body>
-    <p>Hello</p>
-    <p>Welcome To Data School Project</p>
+    <main>
+        <section class="main_container">
+            <section class="home">
+                <section class="header">
+                    <section class="image">
+                        <img src="Assets/Images/Picture1.png" alt="">
+                        <img src="Assets/Images/Picture2.png" alt="">
+                    </section>
+                    <section class="nav_bar">
+                        <img src="Assets/Images/1200px-Hamburger_icon_white.svg.png" alt="">
+                        <ul>
+                            <li><a href="#">Speakers</a></li>
+                            <li><a href="#">Accomodation</a></li>
+                            <li><a href="#">Sponsors</a></li>
+                            <li><a href="#">Register</a></li>
+                        </ul>
+                    </section>
+                </section>
+                <section class="showcase">
+                    <section class="sub_topic">
+                        <p>Department of Statistics, University of Lagos In Conjuction with Statistics Without Borders Presents</p>
+                    </section>
+                    <section class="topic">
+                        <p>
+                            DATA SCIENCE SCHOOL: 
+                        </p>
+                        <p>
+                            Industry Engagement And Applications
+                        </p>
+                    </section>
+                    <section class="related_topics">
+                        <p>Some Related Topics To Be Treated</p>
+                        <section>
+                            <ul>
+                               <li>Deep Learning</li>
+                               <li>Machine Learning</li>
+                               <li>Data Mining</li>
+                               <li>Big Data Analytics</li>
+                               <li>Artificial Intelligence</li>
+                               <li>Data Visualization</li>
+                               <li>DataBase (Including SQL)</li>
+                               <li>Ethics</li> 
+                            </ul>
+                        </section>
+                    </section>
+                    <section class="date_time">
+                        <p><i class="fa-solid fa-location-dot" style="color: #87cefa;"></i>University Of Lagos</p>
+                        <p><i class="fa-regular fa-calendar-days" style="color: #87cefa;"></i>7<sup>th</sup> - 18<sup>th</sup>August, 2023</p>
+                        <p><i class="fa-regular fa-clock" style="color: #87cefa;"></i>9am - 5pm daily</p>
+                    </section>
+                </section>
+            </section>
+            <section class="What_To_Do_And_Requirement"></section>
+            <section class="Speakers" id="Speakers"></section>
+            <section class="register" id="register"></section>
+            <section class="Accomodation" id="Accomodation"></section>
+            <section class="sponsors" id="sponsors"></section>
+        </section>
+    </main>
 </body>
 </html>
